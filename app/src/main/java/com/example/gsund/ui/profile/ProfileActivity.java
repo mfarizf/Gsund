6872119ced217a8721ee1940498dd607836d854c
redcountry15 <nfrostyy17@gmail.com>
2020-01-17 11:13:20 +0700

    implement progress

--- app/src/main/java/com/example/gsund/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/gsund/ui/profile/ProfileActivity.java
@@ -2,12 +2,17 @@ package com.example.gsund.ui.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.gsund.R;
 import com.example.gsund.data.prefs.PreferencesManager;
+import com.example.gsund.ui.profile_progress.ProgressProfileActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -18,6 +23,10 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         preferencesManager = new PreferencesManager(this);
+        Progress();
+    }
 
+    @OnClick(R.id.btn_progress)
+    void Progress(){startActivity(new Intent(ProfileActivity.this, ProgressProfileActivity.class));
     }
 }

a775d807f234859429e82c6b277843c7da4bd69b
annisarhy <analisarahayu@gmail.com>
2020-01-17 15:00:39 +0700

    Tambah Menu Makanan

--- app/src/main/java/com/example/gsund/ui/menumakan/MenuMakan.java
+++ app/src/main/java/com/example/gsund/ui/menumakan/MenuMakan.java
@@ -2,9 +2,13 @@ package com.example.gsund.ui.menumakan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.gsund.R;
+import com.example.gsund.ui.kumpulanmakanan.KumpulanMakan;
+import com.example.gsund.ui.main.MainActivity;
 
 public class MenuMakan extends AppCompatActivity {
 
@@ -12,5 +16,10 @@ public class MenuMakan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_makan);
+    }
+
+    public void lihat_semua(View view) {
+        Intent i = new Intent(MenuMakan.this, KumpulanMakan.class);
+        startActivity(i);
     }
 }

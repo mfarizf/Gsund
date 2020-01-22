fd88030a948a1b78e513ec2a58e7759a9a2d0576
redcountry15 <nfrostyy17@gmail.com>
2020-01-22 10:29:52 +0700

    add section page, fix sebagian layout

--- app/src/main/java/com/example/gsund/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/gsund/ui/profile/ProfileActivity.java
@@ -1,14 +1,17 @@
 package com.example.gsund.ui.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.gsund.R;
+import com.example.gsund.adapter.SectionsPagerAdapter;
 import com.example.gsund.data.prefs.PreferencesManager;
 import com.example.gsund.ui.profile_progress.ProgressProfileActivity;
+import com.google.android.material.tabs.TabLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,9 +26,12 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         preferencesManager = new PreferencesManager(this);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this,getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    @OnClick(R.id.btn_progress)
-    void Progress(){startActivity(new Intent(ProfileActivity.this, ProgressProfileActivity.class));
-    }
+
 }

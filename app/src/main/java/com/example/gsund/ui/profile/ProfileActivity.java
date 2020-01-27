00ad6fc89648a6505085e91888b7d9af5b406f3d
annisarhy <analisarahayu@gmail.com>
2020-01-27 13:20:37 +0700

    Add Layout Timer & update profile

--- app/src/main/java/com/example/gsund/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/gsund/ui/profile/ProfileActivity.java
@@ -3,12 +3,17 @@ package com.example.gsund.ui.profile;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.gsund.R;
+import com.example.gsund.ui.main.MainActivity;
+import com.example.gsund.ui.menumakan.MenuMakan;
 import com.example.gsund.ui.profile.adapter.SectionsPagerAdapter;
 import com.example.gsund.data.prefs.PreferencesManager;
 import com.google.android.material.tabs.TabLayout;
+
+import butterknife.OnClick;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -24,6 +29,10 @@ public class ProfileActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+    @OnClick(R.id.btn_ubahdata)
+    void btn_ubahdata(){
+        startActivity(new Intent(ProfileActivity.this, UbahData.class));
     }
 
 
---
+++ app/src/main/java/com/example/gsund/ui/timer/TimerOlahraga.java
@@ -0,0 +1,16 @@
+package com.example.gsund.ui.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gsund.R;
+
+public class TimerOlahraga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_olahraga);
+    }
+}
